4e95a0ee92f87bbdb6b0a5114367dc20d4b8623e
Jakeline2525 <jakelinesouza@hagmachado.com.br>
2025-10-13 16:47:52 -0300

    Add files via upload

---
+++ render_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Instala as dependências
+pip install -r requirements.txt
+
+# Instala o locale pt_BR para as datas em português
+apt-get update && apt-get install -y locales
+locale-gen pt_BR.UTF-8
+
+# Roda a aplicação Streamlit
+streamlit run dashboard.py --server.port $PORT
