330204e9c93330caf9a5f868f991e80e6bb433ce
Jakeline2525 <jakelinesouza@hagmachado.com.br>
2025-10-14 09:32:45 -0300

    Update render_deploy.sh

--- render_deploy.sh
+++ render_deploy.sh
@@ -3,9 +3,10 @@
 # Instala as dependências
 pip install -r requirements.txt
 
-# Instala o locale pt_BR para as datas em português
+# Instala e configura o locale pt_BR para as datas em português
 apt-get update && apt-get install -y locales
 locale-gen pt_BR.UTF-8
+export LANG=pt_BR.UTF-8
 
 # Roda a aplicação Streamlit
 streamlit run dashboard.py --server.port $PORT
